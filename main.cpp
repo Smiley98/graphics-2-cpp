cd90db77bf4888949552351e7c7f231df13e643d
Smiley98 <smileysofficialcinema@gmail.com>
2026-09-17 08:31:52 -0400

    Completed task 2

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 bool CheckHealth(int health, int required_health)
 {
@@ -19,6 +20,7 @@ void AddByReference(int& number)
 struct Warrior
 {
 	int health;
+	std::string name;
 };
 
 void SetWarriorHealth(Warrior& warrior, int health)
@@ -39,17 +41,22 @@ int main()
 	SetWarriorHealth(warriors[2], 420);
 	SetWarriorHealth(warriors[3], 42);
 	SetWarriorHealth(warriors[4], -9000);
+	warriors[0].name = "David";
+	warriors[1].name = "Michael";
+	warriors[2].name = "Zach";
+	warriors[3].name = "Leija";
+	warriors[4].name = "Connor";
 
 	int required_health = 50;
 	for (int i = 0; i < 5; i++)
 	{
 		if (CheckHealth(warriors[i].health, required_health))
 		{
-			std::cout << "Ready for battle!" << std::endl;
+			std::cout << warriors[i].name << " is ready for battle!" << std::endl;
 		}
 		else
 		{
-			std::cout << "You must rest..." << std::endl;
+			std::cout << warriors[i].name << " must rest..." << std::endl;
 		}
 	}
 
