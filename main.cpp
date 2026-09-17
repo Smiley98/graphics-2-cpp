cf7ef877af991f1cee0233da3a18f92d8868e917
Smiley98 <smileysofficialcinema@gmail.com>
2026-09-17 09:40:37 -0400

    Added native vs standard array

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <array>
 
 struct Person
 {
@@ -7,9 +8,8 @@ struct Person
 	int age;
 };
 
-int main()
+void CreatePeople1(Person people[], int people_count)
 {
-	Person people[3];
 	people[0].name = "David";
 	people[1].name = "Michael";
 	people[2].name = "Connor";
@@ -17,12 +17,30 @@ int main()
 	people[0].age = 21;
 	people[1].age = 31;
 	people[2].age = 28;
+}
+
+void CreatePeople2(std::array<Person, 3>& people)
+{
+	people[0].name = "David";
+	people[1].name = "Michael";
+	people[2].name = "Connor";
+
+	people[0].age = 21;
+	people[1].age = 31;
+	people[2].age = 28;
+}
+
+int main()
+{
+	Person people1[3];
+	std::array<Person, 3> people2;
+	CreatePeople1(people1, 3);
+	CreatePeople2(people2);
 
 	for (int i = 0; i < 3; i++)
 	{
-		std::string name = people[i].name;
-		int age = people[i].age;
-		std::cout << people[i].name << " is " << people[i].age << " years old" << std::endl;
+		std::cout << people1[i].name << " is " << people1[i].age << " years old" << std::endl;
+		std::cout << people2[i].name << " is " << people2[i].age << " years old" << std::endl;
 	}
 	return 0;
 }
